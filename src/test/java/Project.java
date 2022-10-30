3e871c3af23062e09a6d64e0c6880f9bbe75efea
qwer083 <Qwertyiop890>
2022-10-30 18:11:13 +0300

    Initial commit

---
+++ src/test/java/Project.java
@@ -0,0 +1,13 @@
+public class Project {
+    public static void main(String []args) {
+        int a = 5;
+        int b = 2;
+        int c = a/b;
+        int d = a*b;
+        String s = "общее количество программистов";
+        System.out.println("общее количество программистов: " + a);
+        System.out.println("общее количество тестирощиков: " + b);
+        System.out.println("общее количество специалистов поддержки: " + c);
+        System.out.println("общее количество технических специалистов: " + d);
+    }
+}
